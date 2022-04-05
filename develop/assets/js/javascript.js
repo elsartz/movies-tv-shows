c1f64e8b116ecedece8a23ee4943144cf97b9af3
Sophoan Meas <sophoan.meas@hotmail.com>
2022-04-05 03:37:19 -0400

    add dropdown menu

--- develop/assets/js/javascript.js
+++ develop/assets/js/javascript.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
 
+    function fetchMoviesList() {
+
+    }
+
+    // get Top 10 Tv Shows
+    function fetchTvList() {
+
+        apiUrl = `https://imdb-api.com/en/API/Search`
+
+
+    }
 
 
 
@@ -9,7 +20,23 @@ $(document).ready(function() {
 
 
 
+    function navBarDropDownTV() {
 
+        $('.navbar-dropdown').append(`
+   
+    `)
 
+    }
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $('#tv-show-btn').on('click', navBarDropDownTV)
+
+    $('#search-btn').on('click', function() {
+        console.log('search')
+    })
+
+    $('#movies-btn').on('click', function() {
+        console.log('movie')
+    })
 
 });
